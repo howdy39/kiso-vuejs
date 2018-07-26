0e6b18de74cc53f4abe86afb573217519280b224
howdy39 <tatsuya.howdy@gmail.com>
2018-07-26 21:33:49 +0900

    fix: ライフサイクル

--- src/chapter1/main.js
+++ src/chapter1/main.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-undef */
 
+console.group('app');
 const app = new Vue({
   el: '#app',
   data: {
@@ -11,4 +12,46 @@ const app = new Vue({
       alert(event.target);
     },
   },
+  beforeCreate: function() {
+    console.info('beforeCreate');
+  },
+  created: function() {
+    console.info('created');
+  },
+  beforeMount: function() {
+    console.info('beforeMount');
+  },
+  mounted: function() {
+    console.info('mounted');
+  },
+  beforeUpdate: function() {
+    console.info('beforeUpdate');
+  },
+  updated: function() {
+    console.info('updated');
+  },
+  beforeDestroy: function() {
+    console.info('beforeDestroy');
+  },
+  destroyed: function() {
+    console.info('destroyed');
+  },
 });
+console.groupEnd('app');
+
+console.group('app2');
+new Vue({
+  beforeCreate: function() {
+    console.info('beforeCreate');
+  },
+  created: function() {
+    console.info('created');
+  },
+  beforeMount: function() {
+    console.info('beforeMount'); // elがないので呼ばれない
+  },
+  mounted: function() {
+    console.info('mounted'); // elがないので呼ばれない
+  },
+});
+console.groupEnd('app2');
